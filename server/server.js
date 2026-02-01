3f29d59f37a0bc29e9f262cd78b806592b0a4cc4
yash rana <2200520100072@gmail.com>
2026-02-01 14:22:38 +0530

    cors-issuefix

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.use(
   cors({
     origin: "https://lms-wheat-eight.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
